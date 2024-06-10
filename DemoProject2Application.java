7275f1191368b387f1c3ad8ad5b1fb2d85f34c12
Shubhamm124 <99976470+Shubhamm124@users.noreply.github.com>
2024-06-10 13:47:30 +0530

    Add files via upload

---
+++ DemoProject2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject2Application {
+
+	public static void main(String[] args) {
+		// http://localhost:9090/showProductForm
+		// http://localhost:9090/showProductSearchForm
+		SpringApplication.run(DemoProject2Application.class, args);
+	}
+
+}
